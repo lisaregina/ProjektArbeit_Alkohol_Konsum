bd85fc0ba9944f21bffc5e1c29c4932dd5ecedd2
lisaregina <lisa.hiendl.lh@googlemail.com>
2020-06-27 09:14:24 +0200

    In Public ist drinks.csv

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
   return (
     
     <div className="App">
-     < GetData />
+      <h1>Data</h1>
+     
     </div>
   );
 
--- src/GetData.js
+++ src/GetData.js
@@ -1,31 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+
 
 function GetData() {
   function componentDidMount(){
     return fetch('drinks.csv').then((response) =>{
-      return response;                            //Achtung Baustelle!!
+      console.log(response); 
+      return response;
+                                
     })
   }
+
+  // function Convert2Json(){
+  //   let csvToJson = require('convert-csv-to-json');
+  // }
   
 
-  function makeTable(data) {
 
-    document.getComponentById(data, function(fileContent){
-        
-         var resultArray = [];
-
-         var newLineArray = fileContent.map(elem => fileContent.split('\n')); 
-         
-         var splitArray = newLineArray.map(elem => {
-            for (var i = 0; i < newLineArray.length; i++) {
-                 var noCommaArray = splitArray.map(elem => newLineArray[i].split(","));
-                
-         } })
-         console.log(splitArray);
-         
-     }); 
-  }
   return(
     <p>data</p>
   )
