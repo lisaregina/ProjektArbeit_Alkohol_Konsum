d19ce77af3acbdd7f43f6ac5f39d0f6c7642e673
lisaregina <lisa.hiendl.lh@googlemail.com>
2020-06-27 14:25:51 +0200

    csvToJson library added

--- src/GetData.js
+++ src/GetData.js
@@ -1,18 +1,27 @@
 import React from 'react';
 
-
 function GetData() {
+
   function componentDidMount(){
+
+    var csvToJson = require('convert-csv-to-json'); //library CSVtoJSON
+
     return fetch('drinks.csv').then((response) =>{
       console.log(response); 
       return response;
                                 
     })
+    .then(data =>{
+      var fileInputName = data;
+      var fileOutputName = 'drinks.json';
+
+      csvToJson.generateJsonFileFromCsv(fileInputName, fileOutputName);
+      console.log(fileOutputName);
+      return fileOutputName;
+    })
   }
 
-  // function Convert2Json(){
-  //   let csvToJson = require('convert-csv-to-json');
-  // }
+  
   
 
 
