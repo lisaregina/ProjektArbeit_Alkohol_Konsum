5582aba341b3cf9a87a9be6ca3882cd6e229c653
sarah96w <62751561+sarah96w@users.noreply.github.com>
2020-07-23 20:35:18 +0200

    "m"yChart statt ursprünglich "M"yChart

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './App.css';
 import logo from './logo.svg';
-import MyChart  from './MyChart'
+import MyChart  from './myChart'
 import Dummy from "./dummy";
 import { render } from 'react-dom';
 
